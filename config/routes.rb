01a158a5331f15657aafe605560a03167f07e6aa
Yu Shi <yushiys95@gmail.com>
2021-02-23 16:54:28 -0500

    commit tree f8240a7d9b7b03749b7586240b4fb86a92f8dbb1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   resources :students, only: :index
+
+  # The user is able to go to /students/2 to receive information on the student with an id of 2
+  get "students/:id", to: "students#show"
+  
 end
